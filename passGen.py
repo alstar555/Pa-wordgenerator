e8d7f33dc4a7ec808f80b59db875a8b361b8b983
Angelica <67800009+alstar555@users.noreply.github.com>
2021-05-15 23:18:25 -0400

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -1,27 +1,30 @@
 #unique but easy to remember password generator based on what site you're on
-import getpass
 
+import getpass
 #customization
 new = input("new password (y/n): ")
 password_seed = getpass.getpass("Enter your usual password as a seed: ")
 site_name = input("name of website: ")
-#ending = input("trailing chars/nums (s to skip): ")
-#for extra customization add you're own dictionary
+ending = input("trailing chars/nums (s to skip): ")
+
+print("***for security customize the encoding symbols in the code***")
+#for security add you're own dictionary or change the symbols in this symbol_dict 
 symbol_dict = {
-    'a' : '@',
-    'h' : '#',
-    'e' : '3',
-    'i' : '!',
-    'o' : '0',
-    'b' : '6',
-    's' : '$',
-    't' : '+'
+    'a' : '&',
+    'b' : '*'
+    'p' : 'd',
+    'l' : '7',
+    'o' : 'oo',
+    'c' : '(',
+    's' : '5',
+    'g' : '9',
+    'x' : '><'
 }
+# ^^^ customize the symbol_dict
 
 #encoding magic
-# if ending == 's':
-#     ending = ""
-ending = "$!"
+if ending == 'ending = "$!"s':
+     ending = ""
 password_seed = password_seed.capitalize()
 password = site_name + password_seed + ending
 password = password[:1].upper() + password[1:]
@@ -33,7 +36,5 @@ for letter in password:
 if new == "y":
     mysites = open("siteList.txt", "a+")
     mysites.write(site_name + "\n")
-
-
 
 print("Generated Pa$$woRd: ", password)
